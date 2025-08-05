db163edf32993c81da6b683cf1e382a9d3823b36
Amritpal Nagra <amritpalnagrame@gmail.com>
2025-08-05 05:43:57 +0530

    change mod key to CMD for dwm

--- config.def.h
+++ config.def.h
@@ -45,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask // Mod4Mask: Alt, Mod4Mask: CMD
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
