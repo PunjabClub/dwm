8aaf81968b1beaf4098af53280766d241cc678af
Amritpal Nagra <amritpalnagrame@gmail.com>
2025-08-05 17:33:53 +0530

    apply rose pine patch

--- config.def.h
+++ config.def.h
@@ -3,19 +3,28 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char *fonts[]          = { "monospace:size=20" };
+static const char dmenufont[]       = "monospace:size=20";
+
+// https://rosepinetheme.com/palette/
+// - base: Primary background
+// - surface: Secondary background
+// - subtle: Medium Contrast foreground
+// - text: High Contrast foreground
+// - gold: (For now, it's uniqueness provider)
+static const char col_gray1[]       = "#191724"; /* base */
+static const char col_gray2[]       = "#1f1d2e"; /* surface */
+static const char col_gray3[]       = "#908caa"; /* subtle */
+static const char col_gray4[]       = "#e0def4"; /* text */
+static const char col_cyan[]        = "#f6c177"; /* gold */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { "#1f1d2e", col_cyan,  col_cyan  },
 };
 
 /* tagging */
