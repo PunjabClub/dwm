220d6609cf0de59436e0d6b85c27701f8e83566e
Amritpal Nagra <amritpalnagrame@gmail.com>
2025-08-05 18:38:05 +0530

    increase font size further

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "monospace:size=20" };
-static const char dmenufont[]       = "monospace:size=20";
+static const char *fonts[]          = { "monospace:size=25" };
+static const char dmenufont[]       = "monospace:size=25";
 
 // > https://rosepinetheme.com/palette/
 // - base: Primary background
