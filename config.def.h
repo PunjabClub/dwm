8ab2ea955b0f0f66c7cc8d1b9343952d3d3734a7
Amritpal <amritpalnagrame@gmail.com>
2025-08-08 00:23:27 +0530

    make st to open in first window

--- config.def.h
+++ config.def.h
@@ -42,6 +42,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      		instance    title       tags mask     isfloating   monitor */
+	{ "st",  		NULL,       NULL,       1 << 0,       0,           -1 },
 	{ "qutebrowser",  	NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  		NULL,       NULL,       1 << 8,       0,           -1 },
